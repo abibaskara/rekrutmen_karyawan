122df8d9464e0565f470a8f7354075595049f983
abibaskara <abibaskara211@gmail.com>
2022-10-17 10:29:52 +0700

    update-database

--- database/rekrutmen_karyawan (1).sql
+++ database/rekrutmen_karyawan (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 24 Jun 2022 pada 07.14
--- Versi server: 5.7.33
--- Versi PHP: 7.4.19
+-- Generation Time: Oct 17, 2022 at 02:37 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `apply_job`
+-- Table structure for table `apply_job`
 --
 
 CREATE TABLE `apply_job` (
@@ -39,16 +39,18 @@ CREATE TABLE `apply_job` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `apply_job`
+-- Dumping data for table `apply_job`
 --
 
 INSERT INTO `apply_job` (`id_apply_job`, `id_user`, `id_pelamar`, `id_lowongan`, `hasil_test`, `hasil_psikotes`, `status_hasil`, `tgl_lamar`) VALUES
-(3, 2, 1, 3, 80, 100, 'Diterima', '2022-06-23');
+(3, 2, 1, 3, 80, 100, 'Diterima', '2022-06-23'),
+(4, 2, 1, 4, 50, 0, 'Sedang Di Proses', '2022-07-11'),
+(5, 9, 2, 3, 20, 0, 'Tidak Diterima', '2022-09-23');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bahasa`
+-- Table structure for table `bahasa`
 --
 
 CREATE TABLE `bahasa` (
@@ -59,16 +61,17 @@ CREATE TABLE `bahasa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `bahasa`
+-- Dumping data for table `bahasa`
 --
 
 INSERT INTO `bahasa` (`id_bahasa`, `id_user`, `bahasa_asing`, `tingkat_keahlian_bahasa`) VALUES
-(1, 2, 'Inggris', 'Baik');
+(1, 2, 'Inggris', 'Baik'),
+(2, 9, 'Inggris', 'Baik');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `departement`
+-- Table structure for table `departement`
 --
 
 CREATE TABLE `departement` (
@@ -77,7 +80,7 @@ CREATE TABLE `departement` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `departement`
+-- Dumping data for table `departement`
 --
 
 INSERT INTO `departement` (`id_departement`, `nama_departement`) VALUES
@@ -86,7 +89,7 @@ INSERT INTO `departement` (`id_departement`, `nama_departement`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jabatan`
+-- Table structure for table `jabatan`
 --
 
 CREATE TABLE `jabatan` (
@@ -95,7 +98,7 @@ CREATE TABLE `jabatan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `jabatan`
+-- Dumping data for table `jabatan`
 --
 
 INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`) VALUES
@@ -104,7 +107,7 @@ INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `keahlian`
+-- Table structure for table `keahlian`
 --
 
 CREATE TABLE `keahlian` (
@@ -115,19 +118,20 @@ CREATE TABLE `keahlian` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `keahlian`
+-- Dumping data for table `keahlian`
 --
 
 INSERT INTO `keahlian` (`id_keahlian`, `id_user`, `keterampilan`, `tingkat_keahlian_keterampilan`) VALUES
 (1, 2, 'Bahasa Pemrograman PHP', 'Sedang'),
 (2, 2, 'LARAVELs', 'Sangat Baik'),
 (3, 2, 'CODEIGNITER', 'Sangat Baik'),
-(4, 2, 'JQUERY', 'Ahli');
+(4, 2, 'JQUERY', 'Ahli'),
+(5, 9, 'LARAVEL', 'Sangat Baik');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `lowongan`
+-- Table structure for table `lowongan`
 --
 
 CREATE TABLE `lowongan` (
@@ -148,17 +152,17 @@ CREATE TABLE `lowongan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `lowongan`
+-- Dumping data for table `lowongan`
 --
 
 INSERT INTO `lowongan` (`id_lowongan`, `nama_lowongan`, `id_departement`, `id_jabatan`, `max_usia`, `min_pend`, `jurusan_pend`, `salary`, `deskripsi`, `kriteria`, `id_test`, `id_psikotes`, `tgl_close`, `status`) VALUES
-(3, 'Web Developer', 1, 2, 24, 'D4/S1', 'Teknik Informatika', 6000000, 'test', 'Web Development', 3, 3, '2022-06-30', 1),
-(4, 'IT Programmer', 1, 2, 23, 'D3', 'Teknik Informatika', 6000000, 'Test', 'test', 4, 4, '2022-06-30', 1);
+(3, 'Web Developer', 1, 2, 24, 'D4/S1', 'Teknik Informatika', 6000000, 'test', 'Web Development', 3, 3, '2022-09-30', 1),
+(4, 'IT Programmer', 1, 2, 23, 'D3', 'Teknik Informatika', 6000000, 'Test', 'test', 4, 4, '2022-07-31', 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelamar`
+-- Table structure for table `pelamar`
 --
 
 CREATE TABLE `pelamar` (
@@ -185,16 +189,17 @@ CREATE TABLE `pelamar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pelamar`
+-- Dumping data for table `pelamar`
 --
 
 INSERT INTO `pelamar` (`id_pelamar`, `id_user`, `nama_pelamar`, `no_ktp`, `email`, `hp`, `tempat_lahir`, `tgl_lahir`, `jk`, `status_kawin`, `kewarganegaraan`, `agama`, `no_npwp`, `sim`, `no_sim`, `alamat`, `alamat_domisili`, `status_tempat_tinggal`, `foto_pelamar`, `cv`) VALUES
-(1, 2, 'Abi Baskara Atthallah', '3209201709990012', 'abibaskara211@gmail.com', '0822999151278', 'Bandung', '1999-09-17', 'Laki-Laki', 'Lajang', 'WNI', 'Islam', '73128901293810', 'SIM C', '329847230717289', 'Test', 'Test', 'Milik Orang Tua', 'indosat_baru1.png', 'Note_program_Rendy_Jereniko1.pdf');
+(1, 2, 'Abi Baskara Atthallah', '3209201709990012', 'abibaskara211@gmail.com', '0822999151278', 'Bandung', '1999-09-17', 'Laki-Laki', 'Lajang', 'WNI', 'Islam', '73128901293810', 'SIM C', '329847230717289', 'Test', 'Test', 'Milik Orang Tua', 'indosat_baru1.png', 'Note_program_Rendy_Jereniko1.pdf'),
+(2, 9, 'Vallent Perkasa', '21213123', 'vallent@gmail.com', '012837129321', 'Bandung', '1999-09-17', 'Laki-Laki', 'Menikah', 'WNI', 'Islam', '73128901293810', 'SIM C', '329847230717289', 'test', 'test', 'Milik Sendiri', 'Foto_Abi_Baskara_A.jpeg', 'Abi_baskara_atthallah_(1).pdf');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pendidikan`
+-- Table structure for table `pendidikan`
 --
 
 CREATE TABLE `pendidikan` (
@@ -210,18 +215,19 @@ CREATE TABLE `pendidikan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pendidikan`
+-- Dumping data for table `pendidikan`
 --
 
 INSERT INTO `pendidikan` (`id_pendidikan`, `id_user`, `pendidikan_terakhir`, `nama_sekolah`, `kota`, `jurusan`, `start_year`, `end_year`, `nilai_pend`) VALUES
 (1, 2, 'Strata Satu (S1)', 'Politeknik Negeri Jakarta', 'Depok', 'Teknik Informatika', '2022-06', '2022-06', '3'),
 (2, 2, 'SMA/SMK Sederajat', 'SMA Pasundan 1 Bandung', 'Bandung', 'IPAs', '2021-01', '2022-06', '34.5'),
-(3, 2, '', '', '', '', '', '', '');
+(3, 2, '', '', '', '', '', '', ''),
+(4, 9, 'Strata Satu (S1)', 'Politeknik Negeri Jakarta', 'Kota Depok', 'Teknik Informatika', '2022-09', '2022-12', '3.46');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengalaman`
+-- Table structure for table `pengalaman`
 --
 
 CREATE TABLE `pengalaman` (
@@ -239,19 +245,20 @@ CREATE TABLE `pengalaman` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pengalaman`
+-- Dumping data for table `pengalaman`
 --
 
 INSERT INTO `pengalaman` (`id_pengalaman`, `id_user`, `posisi_terakhir`, `perusahaan`, `jabatan_terakhir`, `mulai_bekerja`, `berakhir_bekerja`, `gaji_pokok`, `gaji_insentif`, `fasilitas_terakhir`, `alasan`) VALUES
 (1, 2, 'Web Development', 'PT Anugrah Inti Artha Mandiri', 'Web Development', '2022-06', '2022-06', 5000000, 5000000, 'test', 'test'),
 (2, 2, 'IT Programmer', 'LINTASARTA', 'IT Programmer', '2022-01', '2022-06', 6000000, 6000000, 'test', 'test'),
 (3, 2, 'IT Programmer12', 'LINTASARTA12', 'IT Programmer1', '2022-01', '2022-06', 1000000, 1000000, 'test2', 'test23'),
-(4, 2, 'IT Programmer2', 'LINTASARTA2', 'IT Programmer2', '2022-01', '2022-12', 1200000, 1200000, 'test', 'test');
+(4, 2, 'IT Programmer2', 'LINTASARTA2', 'IT Programmer2', '2022-01', '2022-12', 1200000, 1200000, 'test', 'test'),
+(5, 9, 'Web Development', 'LINTASARTA', 'Web Development', '2022-08', '2022-12', 5400000, 1000000, 'test', 'test');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `psikotes`
+-- Table structure for table `psikotes`
 --
 
 CREATE TABLE `psikotes` (
@@ -266,17 +273,16 @@ CREATE TABLE `psikotes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `psikotes`
+-- Dumping data for table `psikotes`
 --
 
 INSERT INTO `psikotes` (`id_psikotes`, `id_lowongan`, `pertanyaan_psikotes`, `option_a_psikotes`, `option_b_psikotes`, `option_c_psikotes`, `option_d_psikotes`, `kunjaw_psikotes`) VALUES
-(1, 3, 'test pertanyaan psikotes 1', 'kursi', 'meja', 'bantal', 'kasur', 'A'),
 (2, 4, 'test pertanyaan 131', 'ini pilihan a13', 'ini pilihan b13', 'ini pilihan c31', 'ini pilihan d13', 'A');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `soal`
+-- Table structure for table `soal`
 --
 
 CREATE TABLE `soal` (
@@ -291,7 +297,7 @@ CREATE TABLE `soal` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `soal`
+-- Dumping data for table `soal`
 --
 
 INSERT INTO `soal` (`id_soal`, `id_lowongan`, `pertanyaan`, `option_a`, `option_b`, `option_c`, `option_d`, `kunjaw`) VALUES
@@ -306,7 +312,7 @@ INSERT INTO `soal` (`id_soal`, `id_lowongan`, `pertanyaan`, `option_a`, `option_
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `training`
+-- Table structure for table `training`
 --
 
 CREATE TABLE `training` (
@@ -319,17 +325,18 @@ CREATE TABLE `training` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `training`
+-- Dumping data for table `training`
 --
 
 INSERT INTO `training` (`id_training`, `id_user`, `training`, `sertifikasi`, `penyelenggara`, `tahun_pelatihan`) VALUES
 (1, 2, 'CISCO', 'CISCO', 'KSP', '2022-06'),
-(2, 2, 'Junior Web Programmers', 'KSPTIK', 'KSPTIK', '2022-06');
+(2, 2, 'Junior Web Programmers', 'KSPTIK', 'KSPTIK', '2022-06'),
+(3, 9, 'CISCO', 'CISCO', 'KSP', '2022-09');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -344,178 +351,180 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama_user`, `role`, `email`, `no_telp`, `foto`, `password`, `is_active`) VALUES
 (1, 'Abi Baskara Atthallah', 'Admin', 'admin@gmail.com', '082299915127', 'footer-crop.png', '$2y$10$1bLDRcJ0qISuNRT7.PYry.8iu3e0M9YJnLoG1MrzDYzjvfynI/eUi', '1'),
 (2, 'User', 'User', 'user@gmail.com', '0812345678', 'work.png', '$2y$10$6aFVvy2Ruld237BQPe1TCe/AazzIzkAIkUq2Frq8zZYJaYDwa../6', '1'),
 (6, 'Manager Abi Baskara A', 'Manager', 'manager@gmail.com', '082299915127', 'default.png', '$2y$10$NoeLzPUkP.AJ8v47jOiqIe799AngxH63pXPZ0vMfOyJV.qRvxTmIW', '1'),
-(7, 'Supervisor Abi Baskara A', 'Supervisor', 'supervisor@gmail.com', '082299915127', 'graduate.png', '$2y$10$U7uvTmNpXQfOunub1ciVdO8FnwEuqXJz59gEO4OkyAETTbkm2m8BG', '1');
+(7, 'Supervisor Abi Baskara A', 'Supervisor', 'supervisor@gmail.com', '082299915127', 'graduate.png', '$2y$10$U7uvTmNpXQfOunub1ciVdO8FnwEuqXJz59gEO4OkyAETTbkm2m8BG', '1'),
+(8, 'Abi Baskara Atthallah', 'User', 'abibaskara211@gmail.com', '0822999151127', 'default.png', '$2y$10$Bo/6E6a6OQh6Gj2LeI0O0.fOGvUk4sVfC.m5hGdixvnBXDyNXOLS.', '1'),
+(9, 'vallent', 'User', 'vallent@gmail.com', '082299915127', 'default.png', '$2y$10$KrM9X9lOYdw86DecvER.DuJH68RvR7G.w5O0rPBLFtHavwPDxHQ9G', '1');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `apply_job`
+-- Indexes for table `apply_job`
 --
 ALTER TABLE `apply_job`
   ADD PRIMARY KEY (`id_apply_job`);
 
 --
--- Indeks untuk tabel `bahasa`
+-- Indexes for table `bahasa`
 --
 ALTER TABLE `bahasa`
   ADD PRIMARY KEY (`id_bahasa`);
 
 --
--- Indeks untuk tabel `departement`
+-- Indexes for table `departement`
 --
 ALTER TABLE `departement`
   ADD PRIMARY KEY (`id_departement`);
 
 --
--- Indeks untuk tabel `jabatan`
+-- Indexes for table `jabatan`
 --
 ALTER TABLE `jabatan`
   ADD PRIMARY KEY (`id_jabatan`);
 
 --
--- Indeks untuk tabel `keahlian`
+-- Indexes for table `keahlian`
 --
 ALTER TABLE `keahlian`
   ADD PRIMARY KEY (`id_keahlian`);
 
 --
--- Indeks untuk tabel `lowongan`
+-- Indexes for table `lowongan`
 --
 ALTER TABLE `lowongan`
   ADD PRIMARY KEY (`id_lowongan`);
 
 --
--- Indeks untuk tabel `pelamar`
+-- Indexes for table `pelamar`
 --
 ALTER TABLE `pelamar`
   ADD PRIMARY KEY (`id_pelamar`);
 
 --
--- Indeks untuk tabel `pendidikan`
+-- Indexes for table `pendidikan`
 --
 ALTER TABLE `pendidikan`
   ADD PRIMARY KEY (`id_pendidikan`);
 
 --
--- Indeks untuk tabel `pengalaman`
+-- Indexes for table `pengalaman`
 --
 ALTER TABLE `pengalaman`
   ADD PRIMARY KEY (`id_pengalaman`);
 
 --
--- Indeks untuk tabel `psikotes`
+-- Indexes for table `psikotes`
 --
 ALTER TABLE `psikotes`
   ADD PRIMARY KEY (`id_psikotes`);
 
 --
--- Indeks untuk tabel `soal`
+-- Indexes for table `soal`
 --
 ALTER TABLE `soal`
   ADD PRIMARY KEY (`id_soal`);
 
 --
--- Indeks untuk tabel `training`
+-- Indexes for table `training`
 --
 ALTER TABLE `training`
   ADD PRIMARY KEY (`id_training`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `apply_job`
+-- AUTO_INCREMENT for table `apply_job`
 --
 ALTER TABLE `apply_job`
-  MODIFY `id_apply_job` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_apply_job` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `bahasa`
+-- AUTO_INCREMENT for table `bahasa`
 --
 ALTER TABLE `bahasa`
-  MODIFY `id_bahasa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_bahasa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `departement`
+-- AUTO_INCREMENT for table `departement`
 --
 ALTER TABLE `departement`
   MODIFY `id_departement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `jabatan`
+-- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
   MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `keahlian`
+-- AUTO_INCREMENT for table `keahlian`
 --
 ALTER TABLE `keahlian`
-  MODIFY `id_keahlian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_keahlian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `lowongan`
+-- AUTO_INCREMENT for table `lowongan`
 --
 ALTER TABLE `lowongan`
   MODIFY `id_lowongan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `pelamar`
+-- AUTO_INCREMENT for table `pelamar`
 --
 ALTER TABLE `pelamar`
-  MODIFY `id_pelamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pelamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `pendidikan`
+-- AUTO_INCREMENT for table `pendidikan`
 --
 ALTER TABLE `pendidikan`
-  MODIFY `id_pendidikan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pendidikan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `pengalaman`
+-- AUTO_INCREMENT for table `pengalaman`
 --
 ALTER TABLE `pengalaman`
-  MODIFY `id_pengalaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pengalaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `psikotes`
+-- AUTO_INCREMENT for table `psikotes`
 --
 ALTER TABLE `psikotes`
   MODIFY `id_psikotes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `soal`
+-- AUTO_INCREMENT for table `soal`
 --
 ALTER TABLE `soal`
   MODIFY `id_soal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT untuk tabel `training`
+-- AUTO_INCREMENT for table `training`
 --
 ALTER TABLE `training`
-  MODIFY `id_training` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_training` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
